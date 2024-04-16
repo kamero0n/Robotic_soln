58a926d1377e861ae6ad87a9e23d579a60808909
kamero0n <cagomez@usc.edu>
2024-04-16 02:31:44 -0700

    service server to read data from sensor continuously

---
+++ ros2_ws/src/sensor_service/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'sensor_service'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kam',
+    maintainer_email='cagomez@usc.edu',
+    description='Service server to continuously read data from the sensor',
+    license='BSD-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'sensor_service = sensor_service.sensor_service:main',
+        ],
+    },
+)
