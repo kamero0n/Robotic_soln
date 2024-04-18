ac68ddaeacd5ee5a0e5d228ba88b14d94987a9d7
kamero0n <cagomez@usc.edu>
2024-04-18 10:22:46 -0700

    updated service for tcp connection

--- ros2_ws/src/sensor_service/sensor_service/service.py
+++ ros2_ws/src/sensor_service/sensor_service/service.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+import socket
 import numpy as np
 from robotic_soln_interfaces.srv import GetSensorData
 
@@ -8,38 +9,39 @@ class SensorService(Node):
 
     def __init__(self):
         super().__init__('service')
-        self.srv = self.create_service(GetSensorData, 'get_sensor_data', self.handle_sensor_data_request)
-        self.latest_data = np.array([])  # Initialize empty array to store sensor data
-        self.get_logger().info("Service has started and is ready to handle requests.")
+        self.srv = self.create_service(
+            GetSensorData, 'get_sensor_data', self.handle_sensor_data_request)
+        self.socket_setup()
+
+    def socket_setup(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('127.0.0.3', 10000)
+        self.sock.connect(server_address)
+        self.get_logger().info(f"Connected to server at {server_address}")
 
     def read_sensor_data(self, num_samples):
-        data = np.random.rand(3, num_samples)
-        self.get_logger().info(f"Generated sensor data: {data}")
-        return data
+        message_string = str(num_samples)
+        message = message_string.encode()
+        self.sock.sendall(message)
+        byte_data = self.sock.recv(10000 * num_samples)
+        sensor_data = np.frombuffer(byte_data, dtype=np.float64)
+        return sensor_data
     
     def handle_sensor_data_request(self, request, response):
         self.get_logger().info(f"Received request for {request.num_samples} samples.")
         try:
-            num_samples = request.num_samples
-            if num_samples > 0:
-                response.sensor_data = self.read_sensor_data(num_samples).flatten().tolist()
-                response.success = True
-                self.get_logger().info(f"Sending sensor data: {response.sensor_data}")
-            else:
-                response.success = False
-                response.sensor_data = []
-                self.get_logger().warn("Request for zero or negative number of samples.")
+            response.sensor_data = self.read_sensor_data(request.num_samples).tolist()
+            response.success = True
         except Exception as e:
+            self.get_logger().error(f"Failed to read sensor data: {e}")
             response.success = False
-            response.sensor_data = []
-            self.get_logger().error(f"Failed to process request: {str(e)}")
         return response
     
 def main(args=None):
     rclpy.init(args=args)
-    sensor_service = SensorService()
-    rclpy.spin(sensor_service)
-    sensor_service.destroy_node()
+    node = SensorService()
+    rclpy.spin(node)
+    node.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
