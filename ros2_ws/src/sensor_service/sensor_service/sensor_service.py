abee5a0684f2f8c23251d429876d56898e6091fe
kamero0n <cagomez@usc.edu>
2024-04-17 17:27:02 -0700

    fixing set up for running sensor

--- ros2_ws/src/sensor_service/sensor_service/sensor_service.py
+++ ros2_ws/src/sensor_service/sensor_service/sensor_service.py
@@ -5,40 +5,15 @@ import socket
 import numpy as np
 from robotic_soln_interfaces.srv import GetSensorData
 
-class SensorDataClient(Node):
+class SensorService(Node):
+
     def __init__(self):
-        super().__init__('sensor_data_client')
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_address = ('127.0.0.3', 10000)
-        self.client_socket.connect(self.server_address)
-        self.srv = self.create_service(GetSensorData, 'get_sensor_data', self.handle_get_sensor_data)
+        super().__init__('sensor_service')
+        self.srv = self.create_service(
+            GetSensorData, 'get_sensor_data', self.handle_sensor_data_request)
+        self.latest_data = np.array([]) # Initialize empty array to store sensor data
 
-    def handle_get_sensor_data(self, request, response):
-        self.get_logger().info(f'Receiving request for {request.num_samples} samples')
-        # Sending request to sensor server
-        self.client_socket.sendall(str(request.num_samples).encode())
-
-        # Receiving data
-        try:
-            byte_data = self.client_socket.recv(10000)  # Adjust buffer size as needed
-            response.sensor_data = np.frombuffer(byte_data, dtype=np.float64).tolist()
-            response.success = True
-        except Exception as e:
-            self.get_logger().error(f"Failed to receive data: {str(e)}")
-            response.sensor_data = []
-            response.success = False
-
-        return response
-
-    def __del__(self):
-        self.get_logger().info('Closing socket')
-        self.client_socket.close()
-
-def main(args=None):
-    rclpy.init(args=args)
-    sensor_data_client = SensorDataClient()
-    rclpy.spin(sensor_data_client)
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
+    def read_sensor_data(self, num_samples):
+        return np.random.rand(3, num_samples)
+        
+        
--- ros2_ws/src/sensor_service/setup.py
+++ ros2_ws/src/sensor_service/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'sensor_service = sensor_service.sensor_service:main',
+            'sensor = sensor_service.sensor:main',
         ],
     },
 )
